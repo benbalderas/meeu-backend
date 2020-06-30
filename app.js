61386351359feb5ab26eb05bfb104c682cdf195d
Ben Balderas <benbalderas3@gmail.com>
2020-06-29 21:11:56 -0500

    Mongoose and cors config

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-var app = express();
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(`Connected to Mongo. DB name: ${x.connections[0].name}`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
 
-app.use(logger('dev'));
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://meeu.app"],
+    credentials: true,
+  })
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
