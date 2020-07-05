055c3ac0b526fe15af4ac88d76c89333b43df8d3
Ben Balderas <benbalderas3@gmail.com>
2020-07-04 19:59:12 -0500

    edited museums create route to pass the user id

--- routes/museums.js
+++ routes/museums.js
@@ -28,7 +28,8 @@ router.get("/:id", (req, res) => {
 // Create a museum
 router.post("/", veryToken, uploader.single("image"), (req, res) => {
   const image = req.file.path;
-  const museum = { ...req.body, image };
+  const museum = { ...req.body, admin, image };
+  const { _id: admin } = req.user;
 
   Museum.create(museum)
     .then((result) => {
