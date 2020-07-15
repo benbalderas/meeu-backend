e4ee16154499049857cef97521e2da6595c5de0b
Ben Balderas <benbalderas3@gmail.com>
2020-07-15 15:21:53 -0500

    Configure frontend sendFile

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 mongoose
   .connect(process.env.DB, {
@@ -16,32 +16,36 @@ mongoose
     console.log(`Connected to Mongo. DB name: ${x.connections[0].name}`);
   })
   .catch((err) => {
-    console.error("Error connecting to mongo", err);
+    console.error('Error connecting to mongo', err);
   });
 
 const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3001", "https://www.meeu.app"],
+    origin: ['http://localhost:3001', 'https://www.meeu.app'],
     credentials: true,
   })
 );
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const usersRouter = require("./routes/users");
-const artworksRouter = require("./routes/artworks");
-const exhibitsRouter = require("./routes/exhibits");
-const museumsRouter = require("./routes/museums");
+const usersRouter = require('./routes/users');
+const artworksRouter = require('./routes/artworks');
+const exhibitsRouter = require('./routes/exhibits');
+const museumsRouter = require('./routes/museums');
 
-app.use("/", usersRouter);
-app.use("/artworks", artworksRouter);
-app.use("/exhibits", exhibitsRouter);
-app.use("/museums", museumsRouter);
+app.use('/api/', usersRouter);
+app.use('/api/artworks', artworksRouter);
+app.use('/api/exhibits', exhibitsRouter);
+app.use('/api/museums', museumsRouter);
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 module.exports = app;
